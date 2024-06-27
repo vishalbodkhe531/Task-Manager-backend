b8b3f765020729743a13f5f6ed43dfb345ffc978
Vishal Bodkhe <vishalbodkhe@gmail.com>
2024-06-27 12:20:31 +0530

    add cors

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ import taskRoutes from "./Routes/task.routes.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
 import userRoutes from "./Routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 config({ path: "./config/.env" });
 
 databaseConnection();
 
 const server = express();
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 server.use(express.json());
 
